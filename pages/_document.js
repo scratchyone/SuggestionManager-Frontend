2bb26cfd7827dce632f09e101be76f23e3a066e1
scratchyone <scratchywon@gmail.com>
2020-10-10 14:36:31 -0400

    swapped some text to poppins

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
             rel="stylesheet"
           />
           <script
